c334dbf27acd94a60ba030d684a4f3f5a5a38864
Emmanuel <emmanuelbendavid@gmail.com>
2017-11-05 15:13:37 -0300

    ELEMENTS_LIST to independent object and truly thread safed

---
+++ app/src/main/java/com/amaze/filemanager/filesystem/LayoutElementsManager.java
@@ -0,0 +1,94 @@
+package com.amaze.filemanager.filesystem;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.amaze.filemanager.ui.LayoutElementParcelable;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * This is thread safe.
+ * Singleton thread safety source: http://regrecall.blogspot.com.ar/2012/05/java-singleton-pattern-thread-safe.html
+ *
+ * @author Emmanuel
+ *         on 5/11/2017, at 13:55.
+ */
+
+public class LayoutElementsManager implements Parcelable {
+
+    /**
+     * This is not an exact copy of the elements in the adapter
+     *
+     * Iterations should be done synchronizing with this:
+     *
+     * synchronized(getLock()) {
+     *     Iterator i = list.iterator(); // Must be in synchronized block
+     *     while (i.hasNext()) foo(i.next());
+     * }
+     */
+    private List<LayoutElementParcelable> layoutElements;
+
+    public LayoutElementsManager() {}
+
+    public void addLayoutElement(LayoutElementParcelable layoutElement) {
+        layoutElements.add(layoutElement);
+    }
+
+    public LayoutElementParcelable getLayoutElement(int index) {
+        return layoutElements.get(index);
+    }
+
+    public void putLayoutElements(ArrayList<LayoutElementParcelable> layoutElements) {
+        this.layoutElements = Collections.synchronizedList(new ArrayList<>(layoutElements));
+    }
+
+    /**
+     * Get a COPY.
+     */
+    public ArrayList<LayoutElementParcelable> getLayoutElementsClone() {
+        return layoutElements != null? new ArrayList<>(layoutElements):null;
+    }
+
+    public int getLayoutElementSize() {
+        return layoutElements.size();
+    }
+
+    public void clearLayoutElement() {
+        layoutElements.clear();
+    }
+
+    public void removeLayoutElement(int index) {
+        layoutElements.remove(index);
+    }
+
+    private LayoutElementsManager(Parcel in) {
+        layoutElements = Collections.synchronizedList(new ArrayList<>());
+        in.readTypedList(layoutElements, LayoutElementParcelable.CREATOR);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeTypedList(layoutElements);
+    }
+
+    public static final Creator<LayoutElementsManager> CREATOR = new Creator<LayoutElementsManager>() {
+        @Override
+        public LayoutElementsManager createFromParcel(Parcel in) {
+            return new LayoutElementsManager(in);
+        }
+
+        @Override
+        public LayoutElementsManager[] newArray(int size) {
+            return new LayoutElementsManager[size];
+        }
+    };
+
+}
